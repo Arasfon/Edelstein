d2d710b942189dc4627f37b778344d1342dbc653
Egor Boyarkin <egrsfn@gmail.com>
2024-04-06 02:13:14 +0300

    Update RequestSigner test

--- tests/Edelstein.Protocol.Tests/RequestSignerTests.cs
+++ tests/Edelstein.Protocol.Tests/RequestSignerTests.cs
@@ -16,10 +16,10 @@ public class RequestSignerTests
     }
 
     [Theory]
-    [InlineData(0, "1.10.1", 1707614579, """{"asset_version":"30c18961cf5e094e5d68c920d5f74d69","environment":"release"}""",
-        "ghQURYm3lmRz8NmVl7iam9ljUCZBe4Zb3HvuqCvkSOoh3xg0P5cT7DSIE96t2Hah6DexowAYdWSeQFq6S+N1uQ==",
-        "MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAx6zlQl1JSJtwlUbc2t/nePQ5tUXLpcdwJ4VKKPTp3iZCVDgIURDhQ1BrAz108g2xgMib1qKnN0FOA1JxuplQlQIDAQABAkAfy/ZRKGJOY+RcfSWX3C5z6i5TYcsxec4wGO9rkL66P4THAdw8qoEz6ITwyGtBaDcoXqvLliXDeXC5QZ9fDQV5AiEA8bN0RSpHS3FIMdsp37LJVEecWp+KUHZdzGZlaXderA0CIQDTfPdRcU2lJDhd61/mkWZisP/aAv0ms9u7SknbOkWsqQIgDdR85LBEoBZ9UQz/WmN6ja4DTlQur+f1+gop510DN8kCIEvqTsjgWBPQhZ9JD1qvlMYWbTDv47lR1K1NRGe6aipJAiEAlDRODHltMryJZlFrwONcGAIUzYxHFILBI/y767SUl6o=",
-        "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAMes5UJdSUibcJVG3Nrf53j0ObVFy6XHcCeFSij06d4mQlQ4CFEQ4UNQawM9dPINsYDIm9aipzdBTgNScbqZUJUCAwEAAQ==")]
+    [InlineData(100000000000001, "1.11.2", 1712357389, """{"asset_version":"4c921d2443335e574a82e04ec9ea243c","environment":"release"}""",
+        "VmnITQVWE3xl0OHgFQ7efdL7cjq17ndntc3+SeyRFCGyA55PA9ysmiR5tMqplf0kwAYbZAbeKNknhXoC1maUjA==",
+        "MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAwmqQ4T2s3bzw880lTvT+Lz/eP5nrIb9z132WDpvY7U/h6xoq425ji+qoApt0AptvCBwZQDFqY+7LUZQbO3mc5wIDAQABAkAMygQq1QBPUTO2jgWzb86yyVbzmcqhHIjgduyinGJE8LwGbF4+H7/Px+QauZwGbujAmZ42Wdhh40opHA8WjmipAiEA80v3ZItXUbK9+c66UC81wR4YT/3ZmTldxWQE1dQ/dxkCIQDMkTy5Y8JHJo4pIjDsPi/1YNLMYOQXDarmfeu8IMgz/wIgPpcgggFG6YGuVgHU9KPwlEoFvy5zOHQFp65fgBMmcKECIAwnnQoO6V485jl/dJS05TiZNi06nJLaFwt28+jLsdmVAiEAxPle8g3ChpDUpiaaUMacuwmDh6YA5GoRFLHqZ60bT6g=",
+        "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAMJqkOE9rN288PPNJU70/i8/3j+Z6yG/c9d9lg6b2O1P4esaKuNuY4vqqAKbdAKbbwgcGUAxamPuy1GUGzt5nOcCAwEAAQ==")]
     public void RequestSignatureLoop(long userId, string clientVersion, long timestamp, string jsonData, string expectedSignature,
         string privateKey, string publicKey)
     {
