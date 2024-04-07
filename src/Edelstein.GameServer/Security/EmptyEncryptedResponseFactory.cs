50511f67c3d8deeb96fd3b648feca3e96a96f21e
Egor Boyarkin <egrsfn@gmail.com>
2024-04-07 13:32:19 +0300

    Replace null with empty list in EmptyEncryptedResponseFactory, add convenience method

--- src/Edelstein.GameServer/Security/EmptyEncryptedResponseFactory.cs
+++ src/Edelstein.GameServer/Security/EmptyEncryptedResponseFactory.cs
@@ -4,9 +4,12 @@ namespace Edelstein.GameServer.Security;
 
 public class EmptyEncryptedResponseFactory
 {
-    public static EncryptedResponse<object?> Create(ErrorCode code) =>
-        new(new ServerResponse<object?>(code, null));
+    public static EncryptedResponse<List<object>> Create() =>
+        new(new ServerResponse<List<object>>(ErrorCode.Success, []));
 
-    public static EncryptedResponse<object?> Create(GameLibErrorCode code) =>
-        new(new ServerResponse<object?>(code, null));
+    public static EncryptedResponse<List<object>> Create(ErrorCode code) =>
+        new(new ServerResponse<List<object>>(code, []));
+
+    public static EncryptedResponse<List<object>> Create(GameLibErrorCode code) =>
+        new(new ServerResponse<List<object>>(code, []));
 }
