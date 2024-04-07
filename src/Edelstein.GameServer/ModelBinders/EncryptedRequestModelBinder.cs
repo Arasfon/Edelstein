a27d26623eb7638bf629bb59f62037d89354f2c3
Egor Boyarkin <egrsfn@gmail.com>
2024-04-07 03:13:54 +0300

    Disable buffering when binding EncryptedRequest

--- src/Edelstein.GameServer/ModelBinders/EncryptedRequestModelBinder.cs
+++ src/Edelstein.GameServer/ModelBinders/EncryptedRequestModelBinder.cs
@@ -8,10 +8,6 @@ public class EncryptedRequestModelBinder : IModelBinder
 {
     public async Task BindModelAsync(ModelBindingContext bindingContext)
     {
-        if (!bindingContext.HttpContext.Request.Body.CanSeek)
-            bindingContext.HttpContext.Request.EnableBuffering();
-
-        bindingContext.HttpContext.Request.Body.Position = 0;
         using StreamReader sr = new(bindingContext.HttpContext.Request.Body);
         string requestBody = await sr.ReadToEndAsync();
 
