fc5f87626f63357264babba449b8122732c3018c
Egor Boyarkin <egrsfn@gmail.com>
2024-04-12 23:16:24 +0300

    Fix styling

--- src/Edelstein.GameServer/Repositories/ILiveDataRepository.cs
+++ src/Edelstein.GameServer/Repositories/ILiveDataRepository.cs
@@ -6,8 +6,7 @@ namespace Edelstein.GameServer.Repositories;
 public interface ILiveDataRepository
 {
     public Task<UserData> UpdateAfterLive(ulong xuid, Live updatedLive, List<Point> updatedPoints, List<Item> updatedItems,
-        int staminaChange,
-        int experienceChange, int gemChange, List<Character> updatedCharacters, List<Gift> newGifts, LiveMission liveMission,
-        List<uint> clearedMissionIds, List<EventPoint> eventPointUpdates, List<Reward> eventPointRewards,
+        int staminaChange, int experienceChange, int gemChange, List<Character> updatedCharacters, List<Gift> newGifts,
+        LiveMission liveMission, List<uint> clearedMissionIds, List<EventPoint> eventPointUpdates, List<Reward> eventPointRewards,
         RankingChange? rankingChange = null, EventMember? eventMember = null, EventRankingData? eventRankingData = null);
 }
